debe3046835e0b28bce63f4b85a11bf58f5be1ac
Lalitha <chinthalalitha2004@gmil.com>
2025-01-18 10:40:24 +0530

    added src

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+// import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
+import Pic from '../../images/pic2.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description.map((item, index) => (
+                            <div key={index}>
+                                <p key={index}>{item}</p>
+                                <br />
+                            </div>
+                        ))}</SubTitle>
+                        {/* <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton> */}
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+                    <Img src={Pic} alt="hero-image" / >
+                        
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
